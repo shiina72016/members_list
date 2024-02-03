2dd12782f8d66323d63cbc25c843598050d09365
shiina72016 <153628502+shiina72016@users.noreply.github.com>
2024-02-03 15:31:12 +0900

    Model Controller　作成

---
+++ app/Http/Controllers/membersController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Models\members; //membersコントローラーはmembersモデルを使用する
+
+class membersController extends Controller
+{
+    //メンバー一覧
+}
